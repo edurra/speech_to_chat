1963cdf0d3294db187a0011f5944249044261d89
edurra <eolarte944@gmail.com>
2023-06-05 16:49:56 +0200

    Update web.py

--- web.py
+++ web.py
@@ -1,14 +1,20 @@
 from flask import render_template, request
-from flask import Flask, session
+from flask import Flask, session, Response
 import json
 import os
 import uuid
 import subprocess
+import openai
 import azure.cognitiveservices.speech as speechsdk
 app = Flask(__name__)
 
-app.secret_key = os.getenv("FLASK_KEY")
+app.secret_key = os.getenv('FLASK_KEY')
+
 speech_key = os.getenv("SPEECH_KEY")
+
+openai_key = os.getenv('OPENAI_KEY')
+
+openai.api_key = openai_key
 
 def speech_recognize_once_from_file(filename):
     """performs one-shot speech recognition with input from an audio file"""
@@ -39,9 +45,60 @@ def speech_recognize_once_from_file(filename):
             print("Error details: {}".format(cancellation_details.error_details))
     # </SpeechRecognitionWithFile>
     return text
+
+def text_to_audio(path, text):
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region="westeurope")
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=path)
+    # The language of the voice that speaks.
+    speech_config.speech_synthesis_voice_name="en-US-JennyNeural"
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    ssml_text = f'<speak version="1.0" xmlns="https://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US"><voice name="en-US-JennyNeural"><mstts:express-as style="chat">{text}</mstts:express-as></voice></speak>'
+    speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml_text).get()
+    #speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        #print("Speech synthesized for text [{}]".format(text))
+        pass
+    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_synthesis_result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
+
+
+def free_talk(session_messages):
+    initial_message =  f"This is a conversation between you and the user. Try to make the conversation engaging and ask questions if possible. Give short answers, no more than two sentences."
+    messages = [
+        {"role": "system",
+         "content": initial_message}
+    ]
+    
+    #user_message = recognize_from_microphone()
+    #print(f"You: {user_message}")
+    #messages.append({"role": "user", "content": user_message})
+    messages = messages + session_messages
+    assisstant = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, max_tokens=120, temperature = 1)
+    assisstant_message = assisstant['choices'][0]['message']['content']
+    
+    return assisstant_message
+    """
+    messages.append({"role": "assistant", "content": interviewer_message})
+    if len(messages) > max_messages:
+        messages = [messages[0]] + messages[len(messages) - max_messages + 1:len(messages)]
+    """
+
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    if request.method == 'GET':
+        session["messages"] = []
+        session["count"] = 0
+        return render_template('index.html', title='Welcome')
+
+@app.route('/chat', methods = ['POST', 'GET'])
+def chat(max_messages = 7):
     if request.method == 'POST':
+        session["count"] += 1
         print("post received")
         print(request)
         audio_file = request.files['audio_file']
@@ -49,14 +106,46 @@ def index():
         file_path = "tmp/" + identif + ".mp3"
         audio_file.save(file_path)
         file_path_wav = "tmp/" + identif + ".wav"
-        subprocess.call(["ffmpeg", "-i", file_path, file_path_wav])
+        subprocess.run(["ffmpeg", "-i", file_path, file_path_wav], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         text = speech_recognize_once_from_file(file_path_wav)
         os.remove(file_path)
         os.remove(file_path_wav)
-        return json.dumps([{"you": text, "chat": "response"}])
+        session["messages"].append({"role": "user", "content": text})
+        assistant_response = free_talk(session["messages"])
+        session["messages"].append({"role": "assistant", "content": assistant_response})
+
+        if len(session["messages"]) > max_messages:
+            print(session["messages"])
+            session["messages"] =  session["messages"][len(session["messages"]) - max_messages + 1:len(session["messages"])]
+            print(session["messages"])
+        return json.dumps(session["messages"][len(session["messages"])-2:len(session["messages"])])
+
+@app.route('/audio', methods = ['POST', 'GET'])
+def audio():
     if request.method == 'GET':
-        session["messages"] = []
-        return render_template('index.html', title='Welcome')
+        print("get received")
+        print(request)
+        print(session["messages"])
+        text = session["messages"][len(session["messages"])-1]["content"]
+        identif = str(uuid.uuid4())
+        file_path = "tmp/" + identif + ".wav"
+        #file_path = "tmp/test2.wav"
+        text_to_audio(file_path, text)
+
+        response = Response(generate(file_path), mimetype="audio/x-wav")
+        
+        return response
+
+
+def generate(path):
+    with open(path, "rb") as fwav:
+        data = fwav.read(1024)
+        while data:
+            yield data
+            data = fwav.read(1024)
+
+    os.remove(path)
+
 
 if __name__ == "__main__":
-    app.run(port=8000, host="0.0.0.0", debug=True)
+    app.run(port=8000, host="0.0.0.0")
